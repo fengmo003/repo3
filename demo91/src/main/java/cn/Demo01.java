1f76397bd233b2396be719e45ed164c09399d229
fengmo <312867677@qq.com>
2020-05-29 13:21:54 +0800

    这是修改后的代码

--- demo91/src/main/java/cn/Demo01.java
+++ demo91/src/main/java/cn/Demo01.java
@@ -1,10 +1,15 @@
 package cn;
 
 /**
- * 使用idea将工程发送到本地仓库
+ * 克隆远程仓库的代码
+ * 上传修改后的远程代码
  */
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("你好，git");
+    }
+
+    public void test1(){
+        System.out.println("修改添加的方法");
     }
 }
